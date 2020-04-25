ef56545f5df4da563b6f3f6e09c9b3cf253f2f20
thund3rboi <56208524+thund3rboi@users.noreply.github.com>
2020-04-24 19:32:17 -0500

    Newest update

---
+++ src/main/java/dev/micah/command/VanishCommand.java
@@ -0,0 +1,43 @@
+package dev.micah.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VanishCommand implements CommandExecutor {
+
+    private static List<Player> vanishedList = new ArrayList<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            Bukkit.getLogger().info("You cannot run this through console!");
+        }
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            boolean vanished = vanishedList.contains(p);
+            if (vanished) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.showPlayer(p);
+                    vanishedList.remove(p);
+                }
+            } else {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.hidePlayer(p);
+                    vanishedList.add(p);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static List<Player> getVanishedList() {
+        return vanishedList;
+    }
+}
---
+++ src/main/java/dev/micah/runnable/TablistRunnable.java
@@ -0,0 +1,46 @@
+package dev.micah.runnable;
+
+import dev.micah.command.VanishCommand;
+import dev.micah.rank.Rank;
+import dev.micah.utils.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TablistRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (VanishCommand.getVanishedList().contains(p)) {
+                p.setPlayerListName(" ");
+                return;
+            }
+            boolean prefix = false;
+            boolean suffix = false;
+            boolean completed = false;
+            String rank = Rank.getRank(p);
+            if (Rank.getPrefix(rank) != null) {
+                prefix = true;
+            }
+            if (Rank.getSuffix(rank) != null) {
+                suffix = true;
+            }
+            if (suffix && prefix) {
+                p.setPlayerListName(Chat.color(Rank.getPrefix(rank) + " &r" + Rank.getNameColor(rank) + p.getName() + " " + Rank.getSuffix(rank)));
+                completed = true;
+            }
+            if (prefix && !suffix) {
+                p.setPlayerListName(Chat.color(Rank.getPrefix(rank) + " &r" + Rank.getNameColor(rank) + p.getName()));
+                completed = true;
+            }
+            if (suffix && !prefix) {
+                p.setPlayerListName(Chat.color(p.getName() + " " + Rank.getSuffix(rank)));
+                completed = true;
+            }
+            if (!completed) {
+                p.setPlayerListName((p.getName()));
+            }
+        }
+    }
+
+}
