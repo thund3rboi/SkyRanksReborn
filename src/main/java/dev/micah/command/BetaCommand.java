612edccc0138424b19f177723770b3f2cfb8c73d
thund3rboi <56208524+thund3rboi@users.noreply.github.com>
2020-05-07 10:34:14 -0500

    Update BetaCommand.java

--- src/main/java/dev/micah/command/BetaCommand.java
+++ src/main/java/dev/micah/command/BetaCommand.java
@@ -15,7 +15,7 @@ public class BetaCommand implements CommandExecutor {
                 return false;
             }
             /** This key and functions are hidden on github **/
-            if (args[0].equals("NT_!sMw59R%zbqX5G^Z#F?Pr^J=zyD3Bn_EWwvSn2Nw9&Lv@n?zWd&@mAG8ssjYz!AbJjRXvSp=^#s6#U#_ePJvqs%NjbYKnpQQ5q&G@e=2jf*zn%g65va-Mug7?%hq9G^vyySapGJYW!29tcss-MM!n2UyP!7-N?t%5zZfrbn_D3&qsVxXS43US=xQj@mwq?$?Hcgbr^+b#5Mkkt_&j?vF-hcuz8W?9cRm=*-k*eMKnBR-D4%xa!+ajS-B54#kk")) {
+            if (args[0].equals("")) {
                 Bukkit.getLogger().info("[SkyRanksReborn] Activated BETA mode!");
             }
         }
