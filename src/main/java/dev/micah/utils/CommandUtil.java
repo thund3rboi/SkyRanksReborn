1c4f7293977edf08aae6feea67932f9bf4a1e708
thund3rboi <56208524+thund3rboi@users.noreply.github.com>
2020-06-28 10:25:02 -0500

    Cleanup

--- src/main/java/dev/micah/utils/CommandUtil.java
+++ src/main/java/dev/micah/utils/CommandUtil.java
@@ -3,9 +3,7 @@ package dev.micah.utils;
 public class CommandUtil {
 
     public static boolean hasAll(PlayerUtil player, String[] argsIn, int argsNumber, String whatArgsShouldEqual, String permission) {
-        if (argsIn[argsNumber].equalsIgnoreCase(whatArgsShouldEqual) && player.getPlayer().hasPermission(permission)) {
-            return true;
-        }
-        return false;
+        return argsIn[argsNumber].equalsIgnoreCase(whatArgsShouldEqual) && player.getPlayer().hasPermission(permission);
     }
+    
 }
